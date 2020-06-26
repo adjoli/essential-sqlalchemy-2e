22ad90995eb3c7415d4ca81b9bc1d7cd78dadb21
Adao Oliveira <adaum79@gmail.com>
2020-06-26 15:20:52 -0300

    cap06 (parcial)

--- codigo/cap_06.py
+++ codigo/cap_06.py
@@ -1,8 +1,10 @@
 from datetime import datetime
-from sqlalchemy import Table, Column, Integer, Numeric, String, DateTime
+from sqlalchemy import Table, Column, Integer, Numeric, String, DateTime, Boolean, ForeignKey, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
 
 Base = declarative_base()
+
 
 class Cookie(Base):
     __tablename__ = 'cookies'
@@ -14,6 +16,7 @@ class Cookie(Base):
     quantity = Column(Integer())
     unit_cost = Column(Numeric(12, 2))
 
+
 class User(Base):
     __tablename__ = 'users'
 
@@ -23,4 +26,33 @@ class User(Base):
     phone = Column(String(20), nullable=False)
     password = Column(String(25), nullable=False)
     created_on = Column(DateTime(), default=datetime.now)
-    updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
+    updated_on = Column(DateTime(), default=datetime.now,
+                        onupdate=datetime.now)
+
+
+class Order(Base):
+    __tablename__ = 'orders'
+
+    order_id = Column(Integer(), primary_key=True)
+    user_id = Column(Integer(), ForeignKey('users.user_id'))
+    shipped = Column(Boolean(), default=False)
+
+    user = relationship('User', backref=backref('orders', order_by=order_id))
+
+
+class LineItem(Base):
+    __tablename__ = 'line_items'
+
+    line_item_id = Column(Integer(), primary_key=True)
+    order_id = Column(Integer(), ForeignKey('orders.order_id'))
+    cookie_id = Column(Integer(), ForeignKey('cookies.cookie_id'))
+    quantity = Column(Integer())
+    extended_cost = Column(Numeric(12, 2))
+
+    order = relationship('Order', backref=backref(
+        'line_items', order_by=line_item_id))
+
+    cookie = relationship('Cookie', uselist=False)
+
+
+engine = create_engine('sqlite:///:memory:')
